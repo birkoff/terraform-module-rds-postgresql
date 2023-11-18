22684d9b908ed3fce530d9d3a2ca11b2110406cd
Hector R <birkoff.h@gmail.com>
2023-11-18 19:57:00 +0100

    Update data.tf

--- modules/database-role-grant/data.tf
+++ modules/database-role-grant/data.tf
@@ -1,10 +1,10 @@
 data "aws_secretsmanager_secret" "db" {
-  name = "arn:aws:secretsmanager:us-east-1:551892827149:secret:/apps-factory-rds/rds/psql-5l0lFK"
+  name = "arn:aws:secretsmanager:us-east-1:551892827149:secret:/apps-factory-rds/rds/psql"
 }
 data "aws_secretsmanager_secret_version" "credentials" {
   secret_id = data.aws_secretsmanager_secret.db.id
 }
-output "secret_value" {
-  value = data.aws_secretsmanager_secret_version.credentials.secret_string
-  sensitive = false
-}
+# output "secret_value" {
+#   value = data.aws_secretsmanager_secret_version.credentials.secret_string["password"]
+#   sensitive = true
+# }
