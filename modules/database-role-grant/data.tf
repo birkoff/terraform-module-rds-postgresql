2fa05c9fef17c53d2128d034f201e7c74bf22037
Hector R <birkoff.h@gmail.com>
2023-11-18 19:39:54 +0100

    CHINGUE A SU MADRE

---
+++ modules/database-role-grant/data.tf
@@ -0,0 +1,9 @@
+data "aws_secretsmanager_secret" "db" {
+  name = "arn:aws:secretsmanager:us-east-1:551892827149:secret:/apps-factory-rds/rds/psql-5l0lFK"
+}
+data "aws_secretsmanager_secret_version" "credentials" {
+  secret_id = data.aws_secretsmanager_secret.db.id
+}
+output "secret_value" {
+  value = data.aws_secretsmanager_secret_version.credentials.secret_string
+}
